ff67148db0d3432c273daa893b936f3ec93f629d
Juan Fernando <jfdelarosa@outlook.com>
2018-05-12 07:13:50 -0500

    test

--- restore.php
+++ restore.php
@@ -1,5 +1,7 @@
 <?php
 
+$mysqli = new mysqli("mysql.hostmania.es", "u860838189_root", "EuA4XRpsJiBH", "u860838189_api");
+
 $ch = curl_init();
 $url = 'https://raw.githubusercontent.com/jfdelarosa/api/master/backup.sql?token=AA_KOSh1_2JfQcea10_Ur3P91ozoJaTrks5bAHsjwA%3D%3D';
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -8,5 +10,5 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $data = curl_exec($ch);
 curl_close($ch);
 
-echo $data;
+$mysqli->query($data);
 ?>
